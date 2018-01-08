e0ce715b5610d2db88dfe7682f2e285e5654e7dc
chenxz <chenxiaozhen@ieforever.com>
2018-01-08 22:24:58 +0800

    commit rvadapter

---
+++ rvadapter/src/main/java/com/cxz/rvadapter/base/BaseItemDelegate.java
@@ -0,0 +1,15 @@
+package com.cxz.rvadapter.base;
+
+/**
+ * Created by chenxz on 2017/12/28.
+ */
+
+public interface BaseItemDelegate<T> {
+
+    int getItemViewLayoutId();
+
+    boolean isForViewType(T item, int position);
+
+    void convert(BaseViewHolder holder, T t, int position);
+
+}
