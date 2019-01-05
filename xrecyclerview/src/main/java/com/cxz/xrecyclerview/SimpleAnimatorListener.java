c7955c66cfb8713691ebf3777dc12a322de2ee99
chenxz <563859095@qq.com>
2019-01-05 17:38:12 +0800

    commit

---
+++ xrecyclerview/src/main/java/com/cxz/xrecyclerview/SimpleAnimatorListener.java
@@ -0,0 +1,30 @@
+package com.cxz.xrecyclerview;
+
+import android.animation.Animator;
+
+/**
+ * @author chenxz
+ * @date 2019/1/5
+ * @desc
+ */
+public class SimpleAnimatorListener implements Animator.AnimatorListener {
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
